e370a116aae2d5a28943cfc32c2430157a2f8118
diegoCiriano <dgonzalez@tychegroup.es>
2025-04-01 13:06:14 +0200

    endpoints de prueba creados

--- src/main/java/EndpointStubber.java
+++ src/main/java/EndpointStubber.java
@@ -4,9 +4,25 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 public class EndpointStubber {
     //Crea los endpoints mockeados, definiendo la respuesta de manera programatica
     public void setupMocks(){
-        stubFor(post(urlEqualTo("/some/thing"))
+        stubFor(post(urlEqualTo("/reserva/crear"))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withBody("HelloWorld")));
+
+        stubFor(post(urlEqualTo("/reserva/listar"))
+        .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBody("HelloWorld")));
+
+        stubFor(post(urlEqualTo("/huesped/crear"))
+        .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBody("HelloWorld")));
+
+        stubFor(post(urlEqualTo("/huesped/listar"))
+        .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBody("HelloWorld")));
+
     }
 }
